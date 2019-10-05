8855288dd4de3724fe45f555a7cd8f14293b2e91
Paul Teng <plankp@outlook.com>
2019-10-05 16:59:07 -0400

    Add empty controller class

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -0,0 +1,11 @@
+package ca.mcgill.ecse223.quoridor.controller;
+
+/**
+ * This is the controller class for the Quoridor application
+ * 
+ * @author GROUP-9 (Barry Chen, Mohamed Mohamed, Ada Andrei, Paul Teng, Alixe Delabrousse)
+ *
+ */
+public class QuoridorController {
+
+}
