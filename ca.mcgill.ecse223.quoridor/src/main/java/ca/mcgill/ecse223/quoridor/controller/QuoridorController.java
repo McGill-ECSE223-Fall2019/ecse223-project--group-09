3eaa307c67dca3e9d837b5c3706dc9f04247e33f
aliexd <alixe.delabrousse@mail.mcgill.ca>
2019-10-05 20:19:44 -0400

    grabWall and moveWall methods

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -1,11 +1,55 @@
 package ca.mcgill.ecse223.quoridor.controller;
 
+import java.lang.UnsupportedOperationException;
+
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
+import ca.mcgill.ecse223.quoridor.model.*;
+
 /**
  * This is the controller class for the Quoridor application
  * 
  * @author GROUP-9 (Barry Chen, Mohamed Mohamed, Ada Andrei, Paul Teng, Alixe Delabrousse)
  *
  */
+
+
 public class QuoridorController {
+
+/**
+ * 
+ * @author alixe delabrousse 
+ * 
+ * @param player
+ * @param direction
+ * 
+ * This method allows the player to grab one if its walls from its remaining walls pile.
+ * This method enables the use of RotateWall, DropWall, and MoveWall.
+ * 
+ * 
+ */
+	
+	public void grabWall(Player player, Direction direction) {
+		throw new UnsupportedOperationException();
+		
+	}
+	
+	
+	/**
+	 * 
+	 * @author alixe delabrousse
+	 * 
+	 * @param wall
+	 * @param destination
+	 * 
+	 * This methods allows you to place your wall on the board.
+	 * 
+	 * 
+	 */
+	
+	public void moveWall(Wall wall, GamePosition destination) {
+		throw new UnsupportedOperationException();
+		
+	}
+
 
 }
