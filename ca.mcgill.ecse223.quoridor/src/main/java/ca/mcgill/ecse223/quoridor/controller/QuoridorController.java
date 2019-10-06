7ebb064a6035696733c49fab25c6daa6a2b628bf
Ada <adaandrei@hotmail.com>
2019-10-06 10:51:32 -0400

    minor changes

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -1,11 +1,74 @@
 package ca.mcgill.ecse223.quoridor.controller;
 
+import ca.mcgill.ecse223.quoridor.QuoridorApplication;
+import ca.mcgill.ecse223.quoridor.model.Quoridor;
+
 /**
  * This is the controller class for the Quoridor application
  * 
  * @author GROUP-9 (Barry Chen, Mohamed Mohamed, Ada Andrei, Paul Teng, Alixe Delabrousse)
- *
+ * @version 04-10-2019
  */
-public class QuoridorController {
 
+public class String QuoridorController  {
+	
+	/**
+	 * This method allows the user to create a new username.
+	 * 
+	 * @param String user; 
+	 * @return String user; 
+	 * 
+	 * @author Ada Andrei
+	 */
+
+	public static selectUsername() {
+
+		Quoridor quoridor = QuoridorApplication.getQuoridor(); 
+
+		java.lang.UnsupportedOperationExceptiontry {
+			throw new UnsupportedOperationException;
+		} catch (java.lang.UnsupportedOperationException e)
+
+	}
+	
+	/**
+	 * This method allows the user to select a username already created. 
+	 *  
+	 * @param String user; 
+	 * @return String user; 
+	 * 
+	 * @author Ada Andrei
+	 */
+	
+	public static selectUsername() {
+
+		Quoridor quoridor = QuoridorApplication.getQuoridor(); 
+
+		try {
+			throw new UnsupportedOperationException;
+		} catch (java.lang.UnsupportedOperationException e)
+
+	}
+	
+	/**
+	 * This set the total thinking time (minutes and seconds) enforced for both players.
+	 * 
+	 * @param int time;
+	 * @return Time remainingTime; 
+	 * 
+	 * @author Ada Andrei
+	 */
+	
+	public static setTime() {
+
+		Quoridor quoridor = QuoridorApplication.getQuoridor(); 
+
+		try {
+			throw new UnsupportedOperationException;
+		} catch (java.lang.UnsupportedOperationException e)
+
+		return remainingTime; 
+
+	}
 }
+
