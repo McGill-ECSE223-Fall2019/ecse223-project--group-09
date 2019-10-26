4f2ffcb782bcc07f3b317be7b643a8c2f5e4de2d
Paul Teng <plankp@outlook.com>
2019-10-26 06:58:22 -0400

    Add a debug method that displays the stack trace

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/SaveLoadPanel.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/SaveLoadPanel.java
@@ -1,10 +1,18 @@
 package ca.mcgill.ecse223.quoridor.view;
 
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  * A custom component that displays a save and load button
@@ -58,7 +66,30 @@ public class SaveLoadPanel extends JPanel {
      * @author Paul Teng (260862906)
      */
     public void doLoadAction() {
-        System.out.println("Start load...");
+
+    /**
+     * Causes a dialog box to pop up with the full stack trace of the throwable
+     * 
+     * Note: This is a debug-level construct, use it sparringly
+     * 
+     * @param parentComponent The associated parent UI component for this dialog box
+     * @param throwable The throwable whose stack trace is being dumped
+     * 
+     * @author Paul TEng (260862906)
+     */
+    public static void displayThrowableTrace(Component parentComponent, Throwable throwable) {
+        final StringWriter pw = new StringWriter();
+        throwable.printStackTrace(new PrintWriter(pw));
+
+        final JTextArea textArea = new JTextArea(pw.toString(), 20, 50);
+        textArea.setEditable(false);
+        textArea.setTabSize(2);
+
+        final JComponent[] list = {
+            new JLabel("Something bad just happened"),
+            new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED)
+        };
+        JOptionPane.showMessageDialog(parentComponent, list, "Internal Error", JOptionPane.WARNING_MESSAGE);
     }
 
     public static void main(String[] args) {
