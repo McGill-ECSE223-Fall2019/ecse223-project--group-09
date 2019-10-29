aa9908b493beb84017ed0085d2f3597e1d360ac4
Paul Teng <plankp@outlook.com>
2019-10-29 08:14:39 -0400

    Add quit game and resign buttons

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/BoardWindow.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/BoardWindow.java
@@ -1,12 +1,16 @@
 package ca.mcgill.ecse223.quoridor.view;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -50,10 +54,45 @@ public class BoardWindow extends JFrame {
         final JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
+        final JButton btnQuitGame = new JButton("Quit Game");
+        btnQuitGame.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnQuitGame.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+        btnQuitGame.addActionListener(e -> this.onQuitGameButtonClicked());
+
+        final JButton btnResign = new JButton("Resign");
+        btnResign.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnResign.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+        btnResign.addActionListener(e -> this.onResignButtonClicked());
+
         panel.add(saveLoadPanel);
+        panel.add(btnQuitGame);
+
         panel.add(Box.createVerticalGlue());
+
         panel.add(playerInfoPanel);
+
+        // TODO: Remember to add the grab-wall button here
+
+        panel.add(btnResign);
+
+        // Add breathing space between the resign
+        // button and the bottom of the app
+        panel.add(Box.createVerticalStrut(70));
         return panel;
+    }
+
+    /**
+     * This method is called when the quit-game button is clicked
+     */
+    private void onQuitGameButtonClicked() {
+        JOptionPane.showMessageDialog(this, "Quit game is not implemented yet!");
+    }
+
+    /**
+     * This method is called when the resign button is clicked
+     */
+    private void onResignButtonClicked() {
+        JOptionPane.showMessageDialog(this, "Resign is not implemented yet!");
     }
 
     public static void main(String[] args) {
