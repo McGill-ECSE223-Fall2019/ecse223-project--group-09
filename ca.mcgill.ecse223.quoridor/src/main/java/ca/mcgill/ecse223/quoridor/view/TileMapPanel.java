4fc2daee25d467a7d4e6296df846b9af7908ed36
Paul Teng <plankp@outlook.com>
2019-10-30 19:22:01 -0400

    Split tile map panel into its own class

---
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/view/TileMapPanel.java
@@ -0,0 +1,507 @@
+package ca.mcgill.ecse223.quoridor.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.util.Collections;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import ca.mcgill.ecse223.quoridor.controller.Orientation;
+import ca.mcgill.ecse223.quoridor.controller.TOPlayer;
+import ca.mcgill.ecse223.quoridor.controller.TOWall;
+
+/**
+ * This is only the tile part
+ *
+ * @author Paul Teng (260862906)
+ */
+/* package */ class TileMapPanel extends JPanel {
+
+    public static final int SIDE = 9;
+
+    private static final int DIV = 10;
+
+    private static final Color PAWN_CELL_COLOR = Color.lightGray;
+    private static final Color WALL_CELL_COLOR = Color.cyan;
+
+    /**
+     * A function interface for tile related methods
+     *
+     * @author Paul Teng (260862906)
+     */
+    private static interface TileDispatcher {
+
+        /**
+         * Dispatches a tile position
+         *
+         * @param row Row in pawn coordinates
+         * @param col Column in pawn coordinates
+         *
+         * @author Paul Teng (260862906)
+         */
+        public void dispatchTile(int row, int col);
+    }
+
+    /**
+     * A function interface for wall related methods
+     *
+     * @author Paul Teng (260862906)
+     */
+    private static interface SlotDispatcher {
+
+        /**
+         * Dispatches a slot position
+         *
+         * @param row         Row in wall coordinates
+         * @param col         Column in wall coordinates
+         * @param orientation Orientation
+         *
+         * @author Paul Teng (260862906)
+         */
+        public void dispatchSlot(int row, int col, Orientation orientation);
+    }
+
+    public TOPlayer whitePlayer;
+    public TOPlayer blackPlayer;
+
+    public List<TOWall> whiteWalls = Collections.emptyList();
+    public List<TOWall> blackWalls = Collections.emptyList();
+
+    public Orientation junctionOrientation = Orientation.HORIZONTAL;
+
+    public TileMapPanel() {
+        final MouseHandler handler = new MouseHandler();
+        this.addMouseListener(handler);
+        this.addMouseWheelListener(handler);
+        this.addMouseMotionListener(handler);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * We draw here
+     *
+     * @author Paul Teng (260862906)
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        final Dimension d = this.getSize();
+
+        g.setColor(PAWN_CELL_COLOR);
+        g.fillRect(0, 0, d.width, d.height);
+
+        this.drawDividers(g);
+
+        if (this.shouldDrawWhitePlayer()) {
+            this.drawPawn(g, this.whitePlayer.getRow(), this.whitePlayer.getColumn(), Color.white);
+        }
+
+        if (this.shouldDrawBlackPlayer()) {
+            this.drawPawn(g, this.blackPlayer.getRow(), this.blackPlayer.getColumn(), Color.white);
+        }
+
+        for (final TOWall wall : this.whiteWalls) {
+            this.drawWall(g, wall, Color.white);
+        }
+
+        for (final TOWall wall : this.blackWalls) {
+            this.drawWall(g, wall, Color.black);
+        }
+    }
+
+    /**
+     * Check if we should draw the white player
+     *
+     * @return true if we should, false otherwise
+     *
+     * @author Paul Teng (260862906)
+     */
+    private boolean shouldDrawWhitePlayer() {
+        return this.whitePlayer != null && this.whitePlayer.getRow() >= 1 && this.whitePlayer.getRow() <= 9
+                && this.whitePlayer.getColumn() >= 1 && this.whitePlayer.getColumn() <= 9;
+    }
+
+    /**
+     * Check if we should draw the black player
+     *
+     * @return true if we should, false otherwise
+     *
+     * @author Paul Teng (260862906)
+     */
+    private boolean shouldDrawBlackPlayer() {
+        return this.whitePlayer != null && this.blackPlayer.getRow() >= 1 && this.blackPlayer.getRow() <= 9
+                && this.blackPlayer.getColumn() >= 1 && this.blackPlayer.getColumn() <= 9;
+    }
+
+    /**
+     * Draws the pawn at a specific spot
+     *
+     * @param g     Graphics object
+     * @param row   Row in pawn coordinates
+     * @param col   Column in pawn coordinates
+     * @param color Color
+     *
+     * @author Paul Teng (260862906)
+     */
+    private void drawPawn(Graphics g, int row, int col, Color color) {
+        g.setColor(color);
+
+        final Dimension d = this.getSize();
+        final int tileX = d.width / SIDE;
+        final int tileY = d.height / SIDE;
+
+        final int padX = 2 * tileX / DIV;
+        final int padY = 2 * tileY / DIV;
+
+        final int baseX = tileX * (col - 1);
+        final int baseY = tileY * (SIDE - row);
+        g.fillOval(baseX + padX, baseY + padY, tileX - 2 * padX, tileY - 2 * padY);
+    }
+
+    /**
+     * Draws dividers, which are like slots for walls
+     *
+     * @param g Graphics object
+     *
+     * @author Paul Teng (260862906)
+     */
+    private void drawDividers(Graphics g) {
+        g.setColor(WALL_CELL_COLOR);
+
+        final Dimension d = this.getSize();
+        final int tileX = d.width / SIDE;
+        final int tileY = d.height / SIDE;
+
+        final int padX = tileX / DIV;
+        final int padY = tileY / DIV;
+
+        // Only 8 wall slots
+        for (int i = 1; i < SIDE; ++i) {
+            final int baseX = tileX * i;
+            final int baseY = tileY * i;
+
+            g.fillRect(baseX - padX, 0, 2 * padX, d.height);
+            g.fillRect(0, baseY - padY, d.width, 2 * padY);
+        }
+    }
+
+    /**
+     * Draws the wall at a specific spot
+     *
+     * @param g     Graphics object
+     * @param wall  A wall
+     * @param color Color
+     *
+     * @author Paul Teng (260862906)
+     */
+    private void drawWall(Graphics g, TOWall wall, Color color) {
+        final int row = wall.getRow();
+        final int col = wall.getColumn();
+        final Orientation orientation = wall.getOrientation();
+
+        if (orientation == null) {
+            // We are done
+            return;
+        }
+
+        g.setColor(color);
+
+        final Dimension d = this.getSize();
+        final int tileX = d.width / SIDE;
+        final int tileY = d.height / SIDE;
+
+        final int padX = tileX / DIV;
+        final int padY = tileY / DIV;
+
+        switch (orientation) {
+        case VERTICAL: {
+            final int baseX = tileX * col;
+            final int baseY = tileY * (SIDE - row - 1);
+            g.fillRect(baseX - padX, baseY + padY, 2 * padX, 2 * (tileY - padY));
+            break;
+        }
+        case HORIZONTAL: {
+            final int baseX = tileX * (col - 1);
+            final int baseY = tileY * (SIDE - row);
+            g.fillRect(baseX + padX, baseY - padY, 2 * (tileX - padX), 2 * padY);
+            break;
+        }
+        }
+    }
+
+    /**
+     * This is called when the mouse wheel rotates
+     *
+     * @param clicks The amount the wheel rotated
+     */
+    private void onMouseWheelRotate(double clicks) {
+        // TODO
+
+    }
+
+    /**
+     * This is called when any pawn tile is clicked on
+     *
+     * @param row Row in pawn coordinates
+     * @param col Column in pawn coordinates
+     */
+    private void onTileClicked(int row, int col) {
+        // TODO
+        System.out.println("Clicked on tile: " + Character.toString((char) (col - 1 + 'a')) + row);
+    }
+
+    /**
+     * This is called when any wall slot is clicked on
+     *
+     * @param row         Row in wall coordinates
+     * @param col         Column in wall coordinates
+     * @param orientation Orientation
+     */
+    private void onSlotClicked(int row, int col, Orientation orientation) {
+        // TODO
+        System.out.println(
+                Character.toString((char) (col - 1 + 'a')) + row + (orientation == Orientation.VERTICAL ? "v" : "h"));
+    }
+
+    /**
+     * This is called when mouse enters any pawn tile
+     *
+     * @param row Row in pawn coordinates
+     * @param col Column in pawn coordinates
+     */
+    private void onTileEntered(int row, int col) {
+        // TODO
+        System.out.println("Entered tile: " + Character.toString((char) (col - 1 + 'a')) + row);
+    }
+
+    /**
+     * This is called when mouse leaves any pawn tile
+     *
+     * @param row Row in pawn coordinates
+     * @param col Column in pawn coordinates
+     */
+    private void onTileExited(int row, int col) {
+        // TODO
+        System.out.println("Exited tile: " + Character.toString((char) (col - 1 + 'a')) + row);
+    }
+
+    /**
+     * This is called when mouse enters any wall slot
+     *
+     * @param row         Row in wall coordinates
+     * @param col         Column in wall coordinates
+     * @param orientation Orientation
+     */
+    private void onSlotEntered(int row, int col, Orientation orientation) {
+        // TODO
+        System.out.println("Entered: " + Character.toString((char) (col - 1 + 'a')) + row
+                + (orientation == Orientation.VERTICAL ? "v" : "h"));
+    }
+
+    /**
+     * This is called when mouse leaves any wall slot
+     *
+     * @param row         Row in wall coordinates
+     * @param col         Column in wall coordinates
+     * @param orientation Orientation
+     */
+    private void onSlotExited(int row, int col, Orientation orientation) {
+        // TODO
+        System.out.println("Exited: " + Character.toString((char) (col - 1 + 'a')) + row
+                + (orientation == Orientation.VERTICAL ? "v" : "h"));
+    }
+
+    /**
+     * A class that handles mouse related stuff for the Quoridor grid
+     *
+     * @author Paul Teng (260862906)
+     */
+    private final class MouseHandler extends MouseAdapter {
+
+        // ***** State to make magic in tile/slot enter/exit methods *****
+        private int lastX = SIDE + 1;
+        private int lastY = SIDE + 1;
+        private Orientation lastOrientation = null;
+
+        /**
+         * {@inheritDoc}
+         *
+         * Note: handling of this event should be done in
+         * {@link TileMapPanel#onMouseWheelRotate(double) onMouseWheelRotate} instead
+         *
+         * @author Paul Teng (260862906)
+         */
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            TileMapPanel.this.onMouseWheelRotate(e.getPreciseWheelRotation());
+        }
+
+        /**
+         * {@inheritDoc}
+         *
+         * Note: handling of this event should be done in either
+         * {@link TileMapPanel#onTileClicked(int, int) onTileClicked} or
+         * {@link TileMapPanel#onSlotClicked(int, int, Orientation) onSlotClicked}
+         * instead
+         *
+         * @author Paul Teng (260862906)
+         */
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            defaultMouseEventHandler(e, TileMapPanel.this::onTileClicked, TileMapPanel.this::onSlotClicked);
+        }
+
+        /**
+         * {@inheritDoc}
+         *
+         * Note: handling of this event should be done in either
+         * {@link TileMapPanel#onTileEntered(int, int) onTileEntered},
+         * {@link TileMapPanel#onTileExited(int, int) onTileExited},
+         * {@link TileMapPanel#onSlotEntered(int, int, Orientation) onSlotEntered}, or
+         * {@link TileMapPanel#onSlotExited(int, int, Orientation) onSlotExited} instead
+         *
+         * @author Paul Teng (260862906)
+         */
+        @Override
+        public void mouseMoved(MouseEvent e) {
+            this.defaultMouseEventHandler(e, this::dispatchEnterTile, this::dispatchEnterSlot);
+        }
+
+        /**
+         * Based on the mouse event, dispatches either a tile-related method or a
+         * slot-related method
+         *
+         * @param e          The mouse event
+         * @param tileMethod The tile-related method
+         * @param slotMethod The slot-related method
+         *
+         * @author Paul Teng (260862906)
+         */
+        private void defaultMouseEventHandler(MouseEvent e, TileDispatcher tileMethod, SlotDispatcher slotMethod) {
+            final Dimension d = TileMapPanel.this.getSize();
+            final int tileW = d.width / SIDE;
+            final int tileH = d.height / SIDE;
+
+            final int padW = tileW / DIV;
+            final int padH = tileH / DIV;
+
+            final int mouseX = e.getX();
+            final int mouseY = e.getY();
+
+            // Find the correct spot
+            final int col = Math.min(SIDE, mouseX / tileW + 1);
+            final int row = Math.min(SIDE, SIDE - mouseY / tileH);
+
+            // Differentiate between tile and slots
+            final int baseX = tileW * (col - 1);
+            final int baseY = tileH * (SIDE - row);
+
+            final boolean flagLeftX = col > 1 && mouseX < baseX + padW;
+            final boolean flagRightX = col < 9 && mouseX > baseX + tileW - padW;
+            final boolean flagTopY = row < 9 && mouseY < baseY + padH;
+            final boolean flagBottomY = row > 1 && mouseY > baseY + tileH - padH;
+
+            // Try to figure out if slot junctions
+            // should be vertical or horizontal
+            switch (junctionOrientation) {
+            case VERTICAL:
+                if (flagLeftX) {
+                    slotMethod.dispatchSlot(Math.min(SIDE - 1, row), col - 1, Orientation.VERTICAL);
+                    return;
+                }
+                if (flagRightX) {
+                    slotMethod.dispatchSlot(Math.min(SIDE - 1, row), col, Orientation.VERTICAL);
+                    return;
+                }
+                if (flagTopY) {
+                    slotMethod.dispatchSlot(row, Math.min(SIDE - 1, col), Orientation.HORIZONTAL);
+                    return;
+                }
+                if (flagBottomY) {
+                    slotMethod.dispatchSlot(row - 1, Math.min(SIDE - 1, col), Orientation.HORIZONTAL);
+                    return;
+                }
+                break;
+            case HORIZONTAL:
+                if (flagTopY) {
+                    slotMethod.dispatchSlot(row, Math.min(SIDE - 1, col), Orientation.HORIZONTAL);
+                    return;
+                }
+                if (flagBottomY) {
+                    slotMethod.dispatchSlot(row - 1, Math.min(SIDE - 1, col), Orientation.HORIZONTAL);
+                    return;
+                }
+                if (flagLeftX) {
+                    slotMethod.dispatchSlot(Math.min(SIDE - 1, row), col - 1, Orientation.VERTICAL);
+                    return;
+                }
+                if (flagRightX) {
+                    slotMethod.dispatchSlot(Math.min(SIDE - 1, row), col, Orientation.VERTICAL);
+                    return;
+                }
+                break;
+            }
+
+            tileMethod.dispatchTile(row, col);
+        }
+
+        /**
+         * Triggers the {@link TileMapPanel#onSlotEntered(int, int, Orientation)
+         * onSlotEntered} event and the exit events if necessary
+         *
+         * @param row         Row in wall coordinates
+         * @param col         Column in wall coordinates
+         * @param orientation Orientation
+         * 
+         * @author Paul Teng (260862906)
+         */
+        private void dispatchEnterSlot(int row, int col, Orientation orientation) {
+            // If same as previous event, then we never exited, do not generate event
+            if (this.lastX == row && this.lastY == col && this.lastOrientation == orientation) {
+                return;
+            }
+
+            if (this.lastOrientation != null) {
+                TileMapPanel.this.onSlotExited(this.lastX, this.lastY, this.lastOrientation);
+            } else if (this.lastX < SIDE + 1) {
+                TileMapPanel.this.onTileExited(this.lastX, this.lastY);
+            }
+
+            TileMapPanel.this.onSlotEntered((this.lastX = row), (this.lastY = col),
+                    (this.lastOrientation = orientation));
+        }
+
+        /**
+         * Triggers the {@link TileMapPanel#onTileEntered(int, int) onTileEntered} event
+         * and the exit events if necessary
+         *
+         * @param row Row in pawn coordinates
+         * @param col Column in pawn coordinates
+         * 
+         * @author Paul Teng (260862906)
+         */
+        private void dispatchEnterTile(int row, int col) {
+            // If same as previous event, then we never exited, do not generate event
+            if (this.lastX == row && this.lastY == col && this.lastOrientation == null) {
+                return;
+            }
+
+            if (this.lastOrientation != null) {
+                TileMapPanel.this.onSlotExited(this.lastX, this.lastY, this.lastOrientation);
+            } else if (this.lastX < SIDE + 1) {
+                TileMapPanel.this.onTileExited(this.lastX, this.lastY);
+            }
+
+            // tiles do not have an orientation
+            this.lastOrientation = null;
+            TileMapPanel.this.onTileEntered((this.lastX = row), (this.lastY = col));
+        }
+    }
+}
