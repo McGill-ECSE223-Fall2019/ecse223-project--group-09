df91f60e048bf11391e237d02bacee3c7d8eabd5
adaandrei <adaandrei@hotmail.com>
2019-11-28 14:30:05 -0500

    hope its fixed

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -3727,7 +3727,7 @@ public static TOWall grabWall() {
 	 *
 	 * @author Group-9
 	 */
-	public static boolean initiateCheckGameResult(Reader source) throws InvalidLoadException, IOException {
+	public static boolean initiateCheckGameResult(Reader source) throws InvalidLoadException {
 		Quoridor quoridor = QuoridorApplication.getQuoridor();
 		if (!quoridor.hasCurrentGame()) {
 			// Nothing to process
@@ -3739,111 +3739,103 @@ public static TOWall grabWall() {
 			// Nothing to process
 			return false;
 		}
-<<<<<<< HEAD
 		
 		// TODO: check if player repeated move three times (draw condition)		
 		
-		final BufferedReader buffer = new BufferedReader(source);
+		// final Player whitePlayer = new Player(new Time(0, 3, 0), quoridor.getUser(0), 9, Direction.Horizontal);
+		// final Player blackPlayer = new Player(new Time(0, 3, 0), quoridor.getUser(1), 1, Direction.Horizontal);
+		// whitePlayer.setNextPlayer(blackPlayer);
+		// blackPlayer.setNextPlayer(whitePlayer);
 
-		final Player whitePlayer = new Player(new Time(0, 3, 0), quoridor.getUser(0), 9, Direction.Horizontal);
-		final Player blackPlayer = new Player(new Time(0, 3, 0), quoridor.getUser(1), 1, Direction.Horizontal);
-		whitePlayer.setNextPlayer(blackPlayer);
-		blackPlayer.setNextPlayer(whitePlayer);
+		// if (!quoridor.hasCurrentGame()) {
+		// 	game = new Game(GameStatus.Running, MoveMode.PlayerMove, quoridor);
+		// } else {
+		// 	game = quoridor.getCurrentGame();
+		// 	game.setGameStatus(GameStatus.Running);
+		// 	game.setMoveMode(MoveMode.PlayerMove);
+		// }
+		// game.setWhitePlayer(whitePlayer);
+		// game.setBlackPlayer(blackPlayer);
 
-		if (!quoridor.hasCurrentGame()) {
-			game = new Game(GameStatus.Running, MoveMode.PlayerMove, quoridor);
-		} else {
-			game = quoridor.getCurrentGame();
-			game.setGameStatus(GameStatus.Running);
-			game.setMoveMode(MoveMode.PlayerMove);
-		}
-		game.setWhitePlayer(whitePlayer);
-		game.setBlackPlayer(blackPlayer);
+		// final GamePosition initialPosition;
 
-		final GamePosition initialPosition;
+		// // Meaningfull moves only start at index 1 (which
+		// // actually matches up with the corresponding round number)
+		// final String[] whitePlayerMoves;
+		// final String[] blackPlayerMoves;
 
-		// Meaningfull moves only start at index 1 (which
-		// actually matches up with the corresponding round number)
-		final String[] whitePlayerMoves;
-		final String[] blackPlayerMoves;
+		// {
 
-		{
-			// This is the only reading we actually need to do
-			final String line1 = buffer.readLine();
-			final String line2 = buffer.readLine();
-			final boolean whiteStarts;
+		// 	final boolean whiteStarts;
 
-			// Check which player goes first
-			switch (line1.charAt(0)) {
-				case 'W':
-					// Sanity check line2 must start with 'B'
-					if (line2.charAt(0) != 'B') {
-						throw new InvalidLoadException("Bad player color specification: W -> " + line2.charAt(0));
-					}
-					whiteStarts = true;
-					initialPosition = createInitialGamePosition(whitePlayer, blackPlayer, whitePlayer, game);
-					break;
-				case 'B':
-					// Sanity check line2 must start with 'W'
-					if (line2.charAt(0) != 'W') {
-						throw new InvalidLoadException("Bad player color specification: B -> " + line2.charAt(0));
-					}
-					whiteStarts = false;
-					initialPosition = createInitialGamePosition(whitePlayer, blackPlayer, blackPlayer, game);
-					break;
-				default:
-					throw new InvalidLoadException("Bad player color specification: " + line1.charAt(0));
-			}
+		// 	// Check which player goes first
+		// 	switch (line1.charAt(0)) {
+		// 		case 'W':
+		// 			// Sanity check line2 must start with 'B'
+		// 			if (line2.charAt(0) != 'B') {
+		// 				throw new InvalidLoadException("Bad player color specification: W -> " + line2.charAt(0));
+		// 			}
+		// 			whiteStarts = true;
+		// 			initialPosition = createInitialGamePosition(whitePlayer, blackPlayer, whitePlayer, game);
+		// 			break;
+		// 		case 'B':
+		// 			// Sanity check line2 must start with 'W'
+		// 			if (line2.charAt(0) != 'W') {
+		// 				throw new InvalidLoadException("Bad player color specification: B -> " + line2.charAt(0));
+		// 			}
+		// 			whiteStarts = false;
+		// 			initialPosition = createInitialGamePosition(whitePlayer, blackPlayer, blackPlayer, game);
+		// 			break;
+		// 		default:
+		// 			throw new InvalidLoadException("Bad player color specification: " + line1.charAt(0));
+		// 	}
 
-			// Perform default splitting
-			final String[] seq1 = line1.split("\\s*[:,]\\s*");
-			final String[] seq2 = line2.split("\\s*[:,]\\s*");
+		// 	// Perform default splitting
+		// 	final String[] seq1 = line1.split("\\s*[:,]\\s*");
+		// 	final String[] seq2 = line2.split("\\s*[:,]\\s*");
 
-			if (whiteStarts) {
-				whitePlayerMoves = seq1;
-				blackPlayerMoves = seq2;
-			} else {
-				whitePlayerMoves = seq2;
-				blackPlayerMoves = seq1;
-			}
-		}
+		// 	if (whiteStarts) {
+		// 		whitePlayerMoves = seq1;
+		// 		blackPlayerMoves = seq2;
+		// 	} else {
+		// 		whitePlayerMoves = seq2;
+		// 		blackPlayerMoves = seq1;
+		// 	}
+		// }
 
-		int whiteCounter = 0;
-		int blackCounter = 0; 
-		String[] threePosWhite;
-		String[] threePosBlack;
+		// int whiteCounter = 0;
+		// int blackCounter = 0; 
+		// String[] threePosWhite;
+		// String[] threePosBlack;
 
-		//check for duplicate position
-		for (int i=0; i<whitePlayerMoves.length; i++) {
-			for (int j=i+1; j<whitePlayerMoves.length; j++) {
-				if (whitePlayerMoves[i].equals(whitePlayerMoves[j])) {
-					//if there are less than 3 deflections, update the counter
-					if ((j-i) <= 3) { 
-						whiteCounter++; 
-					}
-				}	
-		}
+		// //check for duplicate position
+		// for (int i=0; i<whitePlayerMoves.length; i++) {
+		// 	for (int j=i+1; j<whitePlayerMoves.length; j++) {
+		// 		if (whitePlayerMoves[i].equals(whitePlayerMoves[j])) {
+		// 			//if there are less than 3 deflections, update the counter
+		// 			if ((j-i) <= 3) { 
+		// 				whiteCounter++; 
+		// 			}
+		// 		}	
+		// }
 
-		//check for duplicate positions
-		for (int k= 0; k<blackPlayerMoves.length; k++) {
-			for (int l=k+1; l<blackPlayerMoves.length; l++) {
-				if (blackPlayerMoves[k].equals(blackPlayerMoves[l])) {
-					//if there are less than 3 deflections, update the counter
-					if ((k-l) <= 3) {
-						blackCounter++; 
-					}
-				}	
-			}
-		}
+		// //check for duplicate positions
+		// for (int k= 0; k<blackPlayerMoves.length; k++) {
+		// 	for (int l=k+1; l<blackPlayerMoves.length; l++) {
+		// 		if (blackPlayerMoves[k].equals(blackPlayerMoves[l])) {
+		// 			//if there are less than 3 deflections, update the counter
+		// 			if ((k-l) <= 3) {
+		// 				blackCounter++; 
+		// 			}
+		// 		}	
+		// 	}
+		// }
 
-		if (whiteCounter==3 | blackCounter == 3) {
-			return true; 
-		}
+		// if (whiteCounter==3 | blackCounter == 3) {
+		// 	return true; 
+		// }
+		
 
-=======
-
-		// TODO: check if player repeated move three times (draw condition)
->>>>>>> ee52af77b27db37696994adde36a2442ed64599c
 		// then call setWinner(p) on the correct player
 
 		GamePosition gpos = game.getCurrentPosition();
