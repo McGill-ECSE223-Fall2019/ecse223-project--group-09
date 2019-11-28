99d46505876448a780222413d2f4898f53756439
Paul Teng <plankp@outlook.com>
2019-11-28 06:00:35 -0500

    Pause time countdown when game is not running
    This includes pauses and end-of-games

--- ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
+++ ca.mcgill.ecse223.quoridor/src/main/java/ca/mcgill/ecse223/quoridor/controller/QuoridorController.java
@@ -2995,6 +2995,21 @@ public static TOWall grabWall() {
 		final TimerTask task = new TimerTask() {
 			@Override
 			public void run() {
+				final Game g;
+				final boolean isWhitePlayer;
+				if (player.hasGameAsWhite()) {
+					g = player.getGameAsWhite();
+					isWhitePlayer = true;
+				} else {
+					g = player.getGameAsBlack();
+					isWhitePlayer = false;
+				}
+
+				if (g.getGameStatus() != GameStatus.Running) {
+					// Make sure time only decreases when game is being played
+					return;
+				}
+
 				final Time remTime = player.getRemainingTime();
 				if (remTime.getHours() > 0 || remTime.getMinutes() > 0 || remTime.getSeconds() > 0) {
 					// Subtract time by milliseconds per tick:
@@ -3007,10 +3022,10 @@ public static TOWall grabWall() {
 					// This means the player ran out of time!
 
 					// the other player wins!
-					if (player.hasGameAsBlack()) {
-						QuoridorController.setWinner(player.getGameAsBlack().getWhitePlayer());
+					if (!isWhitePlayer) {
+						QuoridorController.setWinner(g.getWhitePlayer());
 					} else {
-						QuoridorController.setWinner(player.getGameAsWhite().getBlackPlayer());
+						QuoridorController.setWinner(g.getBlackPlayer());
 					}
 
 					// stop the task
